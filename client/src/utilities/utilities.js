a63c1311ebda3734881b3f1aded299b1f906df3b
Anna Józefowicz <anna.jozefowicz.86@gmail.com>
2020-07-10 17:29:40 +0200

    add src files

---
+++ client/src/utilities/utilities.js
@@ -0,0 +1,20 @@
+export const shuffle = (a) => {
+  for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+export const formatTime = (seconds) => {
+  let min = 0;
+  let sec = 0;
+  if (seconds > 59) {
+    min = Math.floor(seconds / 60);
+    sec = seconds % 60;
+  } else {
+    sec = seconds;
+  }
+  sec = (sec > 9) ? sec : "0" + sec;
+  return `${min}:${sec}`;
+}
